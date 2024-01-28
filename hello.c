85356c5c330d70ec5794eadad153b25e70b7cb52
sonys <neural00@daum.net>
2024-01-28 14:04:05 +0900

    My 1st git

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc , char *argv[])
+
+{
+	int i=0;
+	printf("hello\n");
+	printf("No of Argu : %d\n",argc);
+		for (i=0; i<argc; i++)
+		{
+		printf("argument number %d is %s\n", i, argv[i]);
+		}	
+		return 0;
+}
